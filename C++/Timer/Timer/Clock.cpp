20213901aee0b1d16ffe03d42c2ca7dd7321b3a7
cdublu23 <143819899+cdublu23@users.noreply.github.com>
2025-01-07 17:59:43 -0600

    Removed Clock due to errors

--- C++/Timer/Timer/Clock.cpp
+++ C++/Timer/Timer/Clock.cpp
@@ -2,14 +2,11 @@
 #include <iomanip>
 #include <iostream>
 
-
-//press enter to increment time
-
 Clock::Clock(int hours, int minutes, int seconds)
      : _hours{hours}, _minutes{minutes}, _seconds{seconds}{
         if(hours > 23 || hours < 0) throw std::out_of_range("Hours out of range (0-23): " + hours);
         if(minutes > 59 || minutes < 0) throw std::out_of_range("Minutes out of Range(0-59): " + minutes);
-        if(seconds > 50 || seconds < 0) throw std::out_of_range("Seconds out of range(0-59): " + seconds);
+        if(seconds > 59 || seconds < 0) throw std::out_of_range("Seconds out of range(0-59): " + seconds);
      }
 void Clock::print(){
     std::cout << std::setw(2) << std::setfill('0') << _hours << ":"
@@ -24,7 +21,7 @@ void Clock::tic(){
          if(_minutes == 60){
             _minutes = 0;
             _hours++;
-                if(_hours >= 23){
+                if(_hours == 23){
                     _hours = 0;
                 }
 
